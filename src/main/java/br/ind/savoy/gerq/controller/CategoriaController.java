b356f0e144265d3e88cd4c692dab4c8623fbfa06
waltenesjr <waltenesjr@live.com>
2017-09-15 18:53:08 -0300

    filtro CORS

--- src/main/java/br/ind/savoy/gerq/controller/CategoriaController.java
+++ src/main/java/br/ind/savoy/gerq/controller/CategoriaController.java
@@ -5,11 +5,7 @@ import java.util.List;
 import br.ind.savoy.gerq.model.Categoria;
 import br.ind.savoy.gerq.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CategoriaController {
---
+++ src/main/java/br/ind/savoy/gerq/filter/CORSFilter.java
@@ -0,0 +1,27 @@
+package br.ind.savoy.gerq.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by waltenes on 15/09/17.
+ */
+public class CORSFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+            response.addHeader("Access-Control-Max-Age", "1");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
